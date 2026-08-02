512a715073d5279195f9f656030bb99a205ae397
Brian Keen <brian.keen@escaperoommystery.com>
2026-08-02 09:42:10 -0400

    Test native script tag for Bookeo widget

--- app/gift-vouchers/checkout/GiftVoucherCheckoutClient.tsx
+++ app/gift-vouchers/checkout/GiftVoucherCheckoutClient.tsx
@@ -59,9 +59,9 @@ export default function GiftVoucherCheckoutClient() {
         bookHref={bookHref}
       />
 
-      <Script
+      <script
+        type="text/javascript"
         src="https://bookeo.com/widget.js?a=415686T7W9919DC0FEE2A6&startmode=buyvoucher"
-        strategy="afterInteractive"
       />
 
       <main className="min-h-screen bg-white px-6 py-12">
