263f15666735c0029b5ae883b3f983de03f36924
Brian Keen <brian.keen@escaperoommystery.com>
2026-08-03 02:00:22 -0400

    Add Bookeo test page

---
+++ app/test-bookeo/page.tsx
@@ -0,0 +1,10 @@
+export default function TestBookeoPage() {
+  return (
+    <main style={{ padding: "40px" }}>
+      <script
+        src="https://bookeo.com/widget.js?a=415686T7W9919DC0FEE2A6&startmode=buyvoucher"
+        type="text/javascript"
+      ></script>
+    </main>
+  );
+}
