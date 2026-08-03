2a8abfa839504c2b4f2a4a3daf84f70d83502d08
Brian Keen <brian.keen@escaperoommystery.com>
2026-08-03 12:42:05 -0400

    Place Bookeo script statically per support advice 3

--- app/gift-vouchers/checkout/page.tsx
+++ app/gift-vouchers/checkout/page.tsx
@@ -1,28 +1,12 @@
-import LocationHeader from "../../components/LocationHeader";
-
 export default function GiftVoucherCheckoutPage() {
   return (
-    <>
-      {/* We hard-code Cherry Hill for this test to remove all client complexity */}
-      <LocationHeader
-        locationName="Cherry Hill"
-        locationSubtitle="New Jersey"
-        homeHref="/locations/cherry-hill"
-        roomsHref="/locations/cherry-hill/rooms"
-        bookHref="/locations/cherry-hill/book-now"
+    <main style={{ padding: "40px" }}>
+      <div id="bookeo_position"></div>
+
+      <script
+        type="text/javascript"
+        src="https://bookeo.com/widget.js?a=415686T7W9919DC0FEE2A6&startmode=buyvoucher"
       />
-
-      <main className="min-h-screen bg-white px-6 py-12">
-        <div className="mx-auto max-w-6xl">
-          <div id="bookeo_position"></div>
-
-          {/* Pure static script – exactly like the WordPress page */}
-          <script
-            type="text/javascript"
-            src="https://bookeo.com/widget.js?a=415686T7W9919DC0FEE2A6&startmode=buyvoucher"
-          />
-        </div>
-      </main>
-    </>
+    </main>
   );
 }
