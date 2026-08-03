e8a6f26c3812f1cee4de4af233abdc5c286190e2
Brian Keen <brian.keen@escaperoommystery.com>
2026-08-03 02:11:18 -0400

    Test client-only Bookeo page

--- app/test-bookeo/page.tsx
+++ app/test-bookeo/page.tsx
@@ -1,10 +1,23 @@
+"use client";
+
+import { useEffect } from "react";
+
 export default function TestBookeoPage() {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src =
+      "https://bookeo.com/widget.js?a=415686T7W9919DC0FEE2A6&startmode=buyvoucher";
+    script.async = true;
+    document.body.appendChild(script);
+
+    return () => {
+      script.remove();
+    };
+  }, []);
+
   return (
     <main style={{ padding: "40px" }}>
-      <script
-        src="https://bookeo.com/widget.js?a=415686T7W9919DC0FEE2A6&startmode=buyvoucher"
-        type="text/javascript"
-      ></script>
+      <h1>Bookeo Test</h1>
     </main>
   );
 }
