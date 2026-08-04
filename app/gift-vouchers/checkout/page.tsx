3c603b4bafba83ea6952df8497aaca8f7ea32d50
Brian Keen <brian.keen@escaperoommystery.com>
2026-08-03 20:25:30 -0400

    Place Bookeo script statically per support advice 5

--- app/gift-vouchers/checkout/page.tsx
+++ app/gift-vouchers/checkout/page.tsx
@@ -1,18 +1,24 @@
+"use client";
+
+import { useEffect } from "react";
+
 export default function GiftVoucherCheckoutPage() {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src =
+      "https://bookeo.com/widget.js?a=415686T7W9919DC0FEE2A6&startmode=buyvoucher";
+    script.async = true;
+
+    document.body.appendChild(script);
+
+    return () => {
+      script.remove();
+    };
+  }, []);
+
   return (
     <main style={{ padding: "40px" }}>
-      <img
-        src="/images/rooms/area51-action-01.jpg"
-        alt="Area 51"
-        style={{ maxWidth: "100%", height: "auto" }}
-      />
-
       <div id="bookeo_position"></div>
-
-      <script
-        type="text/javascript"
-        src="https://bookeo.com/widget.js?a=415686T7W9919DC0FEE2A6&startmode=buyvoucher"
-      />
     </main>
   );
 }
