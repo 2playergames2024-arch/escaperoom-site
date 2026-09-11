0c21a77430861b3ebb203950035fc928e14435b3
Brian Keen <brian.keen@escaperoommystery.com>
2026-09-11 03:37:50 -0400

    Allow Meta Pixel in content security policy

--- next.config.ts
+++ next.config.ts
@@ -116,11 +116,11 @@ const securityHeaders = [
     key: "Content-Security-Policy",
     value: [
       "default-src 'self'",
-      "script-src 'self' 'unsafe-inline' https://bookeo.com https://*.bookeo.com https://www.clarity.ms https://*.clarity.ms https://www.googletagmanager.com",
+      "script-src 'self' 'unsafe-inline' https://bookeo.com https://*.bookeo.com https://www.clarity.ms https://*.clarity.ms https://www.googletagmanager.com https://connect.facebook.net",
       "style-src 'self' 'unsafe-inline'",
       "img-src 'self' data: blob: https:",
       "font-src 'self' data:",
-      "connect-src 'self' https://bookeo.com https://*.bookeo.com https://*.clarity.ms https://c.bing.com https://*.google-analytics.com https://*.analytics.google.com https://www.googletagmanager.com https://*.g.doubleclick.net https://*.google.com https://pagead2.googlesyndication.com",
+      "connect-src 'self' https://bookeo.com https://*.bookeo.com https://*.clarity.ms https://c.bing.com https://*.google-analytics.com https://*.analytics.google.com https://www.googletagmanager.com https://*.g.doubleclick.net https://*.google.com https://pagead2.googlesyndication.com https://www.facebook.com",
       "frame-src 'self' https://bookeo.com https://*.bookeo.com https://www.googletagmanager.com",
       "form-action 'self' https://accept.authorize.net https://test.authorize.net",
       "object-src 'none'",
